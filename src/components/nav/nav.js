c8f9dd88a412cf21b480f399df3e5d5bf24a20c8
Aziz Gaparov <azizgithubacc@gmail.com>
2022-04-06 14:13:07 +0600

    chage link in nav.js

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -5,11 +5,11 @@ function Nav() {
         <head><link href="https://fonts.googleapis.com/css2?family=Rubik+Wet+Paint&display=swap" rel="stylesheet"/></head>
             <div>AcCeSsoRies StyLe</div>
             <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Categories</a></li>
-                <li><a href="#">about</a></li>
-                <li><a href="#">products</a></li>
-                <li><a href="#">contacts</a></li>
+                <li><a href="/">Home</a></li>
+                <li><a href="/">Categories</a></li>
+                <li><a href="/">about</a></li>
+                <li><a href="/">products</a></li>
+                <li><a href="/">contacts</a></li>
             </ul>
         </nav>
      );
