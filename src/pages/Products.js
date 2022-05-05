8676c79d1c00332f7c16a440b559b57b33e80f46
Aziz Gaparov <azizgithubacc@gmail.com>
2022-05-05 15:26:54 +0600

    added design to products as price, cards, buttons

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -17,74 +17,74 @@ function Products() {
         {
             imageURL: "/IMG/1.jpg",
             title: 'Info about this products of accessories',
-            about: 'Accessory. Although it’s a word that simply means “a thing which can be added to something else in order to make it more useful, versatile, or attractive”',
+            about: 'Accessory. Although it’s a word that simply means “a thing which can be added to something else in order”',
             price: '$10'
         },
         {
             imageURL: "/IMG/2.png",
-            title: 'Info about this products of accessories',
-            about: 'Accessory. Although it’s a word that simply means “a thing which can be added to something else in order to make it more useful, versatile, or attractive”',
-            price: '$10'
+            title: 'Once we start with the basic premise that yes',
+            about: 'Bracelets evolved over time from good luck charm to status symbol to the modern style accessory for men.',
+            price: '$12'
         },
         {
             imageURL: "/IMG/3.jpg",
-            title: 'Info about this products of accessories',
-            about: 'Accessory. Although it’s a word that simply means “a thing which can be added to something else in order to make it more useful, versatile, or attractive”',
-            price: '$10'
+            title: 'The role of a well-chosen bracelet',
+            about: 'Depending on which part of the globe you call home, the idea of men wearing a bracelet is either normal or plain unheard of.',
+            price: '$12'
         },
         {
             imageURL: "/IMG/4.jpg",
-            title: 'Info about this products of accessories',
+            title: 'Definitely a distinct part of the look.',
             about: 'Accessory. Although it’s a word that simply means “a thing which can be added to something else in order to make it more useful, versatile, or attractive”',
             price: '$10'
         },
         {
             imageURL: "/IMG/5.jpg",
-            title: 'Info about this products of accessories',
-            about: 'Accessory. Although it’s a word that simply means “a thing which can be added to something else in order to make it more useful, versatile, or attractive”',
+            title: 'Metallic and upscale looking is a natural fit',
+            about: 'Bracelets, like watches, can be worn daily. Some are dressier and suit more formal occasions. The variety of materials',
             price: '$10'
         },
         {
             imageURL: "/IMG/6.jpg",
-            title: 'Info about this products of accessories',
-            about: 'Accessory. Although it’s a word that simply means “a thing which can be added to something else in order to make it more useful, versatile, or attractive”',
-            price: '$10'
+            title: 'Sorts of bracelets with a rough-edged',
+            about: 'Brass and hemp. A bracelet is a versatile accessory that  reflects your personality and brings out the color in your outfits.',
+            price: '$11'
         },
         {
             imageURL: "/IMG/7.jpg",
-            title: 'Info about this products of accessories',
-            about: 'Accessory. Although it’s a word that simply means “a thing which can be added to something else in order to make it more useful, versatile, or attractive”',
-            price: '$10'
+            title: 'Emphasizing your overall look.',
+            about: 'The five points listed below are the reasons why a man might consider strapping a shiny piece of metal, nylon or leather on his wrist.',
+            price: '$15'
         },
         {
             imageURL: "/IMG/8.jpg",
-            title: 'Info about this products of accessories',
-            about: 'Accessory. Although it’s a word that simply means “a thing which can be added to something else in order to make it more useful, versatile, or attractive”',
-            price: '$10'
+            title: 'As with necklaces, it"s good',
+            about: 'Accessories are visual cues to your personality. Interesting accessories always draw attention. You might find strangers start approaching you more often when you wear a bracelet',
+            price: '$14'
         },
         {
             imageURL: "/IMG/9.jpg",
-            title: 'Info about this products of accessories',
-            about: 'Accessory. Although it’s a word that simply means “a thing which can be added to something else in order to make it more useful, versatile, or attractive”',
-            price: '$10'
+            title: 'A last note that"s going to be true',
+            about: 'Wearing a bracelet can be a great conversation starter. It could be the excuse the pretty woman in a cafe needs to strike up a conversation.',
+            price: '$11'
         },
         {
             imageURL: "/IMG/10.jpg",
-            title: 'Info about this products of accessories',
-            about: 'Accessory. Although it’s a word that simply means “a thing which can be added to something else in order to make it more useful, versatile, or attractive”',
-            price: '$10'
+            title: 'In both cases, however, notice',
+            about: 'In a world where everyone looks uniform, wearing an eye-catching accessory sets you apart and makes you appear more interesting.',
+            price: '$13'
         },
         {
             imageURL: "/IMG/11.jpg",
-            title: 'Info about this products of accessories',
-            about: 'Accessory. Although it’s a word that simply means “a thing which can be added to something else in order to make it more useful, versatile, or attractive”',
-            price: '$10'
+            title: 'Bracelet sliding back and forth gets old fast.',
+            about: 'It attracts attention in a good way. Perhaps you:re on a beach and the only accessory you can wear is a bracelet.',
+            price: '$13'
         },
         {
             imageURL: "/IMG/12.jpg",
-            title: 'Info about this products of accessories',
-            about: 'Accessory. Although it’s a word that simply means “a thing which can be added to something else in order to make it more useful, versatile, or attractive”',
-            price: '$10'
+            title: 'Lighter stuff like rope-and-bead pieces can have a little',
+            about: 'You"re able to share your appreciation for art history and beautiful hand made artifacts. You could connect with a business acquaintance just by relating how you picked',
+            price: '$12'
         },
     ]   
 
@@ -92,9 +92,9 @@ function Products() {
         <main className="d-flex flex-column">
             <div>
                 <Nav/>
-              ` <Header fone='/img_coverImage/3.jpg' tColor='orange'/>
+                <Header fone='/img_coverImage/3.jpg' tColor='orange'/>
             </div>
-            <div className="mainPage">
+            <div className="mainPage productPage">
                 <div style={styleMain}>
                     {
                         products.map(obj => (
--- src/pages/card/Card.js
+++ src/pages/card/Card.js
@@ -1,20 +1,21 @@
 
 import "./Card.css"
+import price from "../../assets/price3.png"
 
 function Card(props) {
     return ( 
-        <div className="card d-flex flex-column justify-between">
+        <div className="card d-flex flex-column justify-between" style={{backgroundColor: 'white'}}>
             <img src={props.urlImage} className="image" alt="ups..."></img>
             <div>
                 <h4 className="what">{props.title}</h4>
                 <p className="info">
                     {props.info}
                 </p>
-                <p className="d-flex justify-between price">
+                <p className="d-flex justify-between align-center ">
                     <div><b>PRICE:</b></div>
-                    <div>{props.price}</div>
+                    <div><span className="priceNum">{props.price}</span><img src={price} className="priceImage"/></div>
                 </p>
-                <div className="btns d-flex justify-around">
+                <div className="btns d-flex justify-around mb-10">
                     <button className="buy">BUY</button>
                     <button className="save">SAVE</button>
                 </div>
